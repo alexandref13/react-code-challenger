3ab1f4ea79e2f4c7188ea5d6ee27e5b85fa87f62
Alexandre Fernandes <alexfernandes199922@gmail.com>
2022-10-14 19:55:25 -0300

    fix: documents.tsx

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -9,15 +9,16 @@ import Document, {
 } from "next/document";
 
 export default class MyDocument extends Document {
-  // Usar a fonte dentro do Head
-
   render(): JSX.Element {
     return (
-      <Html lang="pt">
+      <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-          <meta name="description" content="" />
+          <meta
+            name="description"
+            content="Test wiht Material Ui and Next.js"
+          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
